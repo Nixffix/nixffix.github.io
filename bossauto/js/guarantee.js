3c3516ff7fc315d37d1c7fe45fdabaf7d291b909
Nixffix <emowisayasod96@gmail.com>
2025-05-04 09:24:36 +0300

    Delete bossauto directory

--- bossauto/js/guarantee.js
+++
@@ -1,5 +0,0 @@
-// Скролл к блоку с контактами по клику
-
-document.querySelector('.gar__scroll').addEventListener('click', event => {
-    document.querySelector(".contact").scrollIntoView({ block: "start", behavior: "smooth" });
-})
